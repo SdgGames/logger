2aea4d5e881bb7a3fc9afe61f6b7b269d102e97f
sdggames <cicd@sdggamecollective.com>
2022-09-10 02:19:46 -0500

    Add console integration with realtime print level changes

--- logger.gd
+++ logger.gd
@@ -56,23 +56,23 @@
 #         logger.critical("Divided by an ocelot error! Segfault immanent")
 #
 # License: MIT
-
 extends Node
 
 # Levels of debugging available
-class Level:
-	const DEBUG = 0
-	const INFO = 1 # default
-	const WARNING = 2
-	const ERROR = 3
-	const CRITICAL = 4
+enum Level { DEBUG = 0, INFO = 1, WARNING = 2, ERROR = 3, CRITICAL = 4 }
 
 # Built in time formatters
-class TimeFormat:
-	const NONE = 0
-	const ELAPSED = 1
-	const TIME = 2
-	const DATETIME = 3 # default
+enum TimeFormat { NONE = 0, ELAPSED = 1, TIME = 2, DATETIME = 3 }
+
+
+func _ready():
+	if $"/root".has_node("Console"):
+		Console.add_command("set_print_level", self, 'set_level')\
+				.set_description("Sets the output level for printing. Debug level is the most verbose. " +\
+					"Each level contains the levels above.")\
+				.add_argument("Level", typeof(1), "Debug = 0, Info = 1, Warning = 2, Error = 3, Critical = 4")\
+				.register()
+
 
 # Print to stdout?
 var print_stdout = true setget set_print_stdout, get_print_stdout
@@ -82,6 +82,7 @@ func set_print_stdout(value):
 	assert(value in [true, false])
 	print_stdout = value
 
+
 # Logging level.
 var level = Level.INFO setget set_level, get_level
 func get_level():
@@ -89,6 +90,7 @@ func get_level():
 func set_level(value):
 	assert(level in [Level.DEBUG, Level.INFO, Level.WARNING, Level.ERROR, Level.CRITICAL])
 	level = value
+
 
 # Logging to file.
 var file = null
@@ -114,6 +116,7 @@ var time_format_func = funcref(self, "format_time_datetime") setget set_time_for
 func set_time_format_func(value):
 	time_format_func = value
 
+
 var time_format = TimeFormat.DATETIME setget set_time_format
 func set_time_format(value):
 	if value == TimeFormat.NONE:
@@ -127,22 +130,25 @@ func set_time_format(value):
 	else:
 		assert(false) # Bad time format used.
 
-# --- Time formatters for use by the logger.
 
+# --- Time formatters for use by the logger.
 func format_time_none():
 	return ""
-	
+
+
 func format_time_elapsed():
 	return "[%s] " % _format_elapsed()
+
 
 func format_time_time():
 	return "[%s] " % _format_time()
 
+
 func format_time_datetime():
 	return "[%s %s] " % [_format_date(), _format_time()]
 
-# --- General time formatting functions
 
+# --- General time formatting functions
 func _format_time():
 	"""Not used directly, but might come in useful"""
 	var time = OS.get_time()
@@ -150,6 +156,7 @@ func _format_time():
 	var ms = OS.get_ticks_msec() % 1000
 
 	return "%02d:%02d:%02d.%03d" % [time["hour"], time["minute"], time["second"], ms]
+
 
 func _format_elapsed():
 	"""Not used directly, but might come in useful"""
@@ -160,50 +167,68 @@ func _format_elapsed():
 	var h = int(time / 3600000)
 
 	return "%d:%02d:%02d.%03d " % [h, m, s, ms]
+
+
 func _format_date():
 	"""Not used directly, but might come in useful"""
 	var date = OS.get_date()
 
 	return "%d-%02d-%02d" % [date["year"], date["month"], date["day"]]
 
-# --- Message writing methods
 
+# --- Message writing methods
 func debug(data):
 	"""Debugging message"""
 	if level == Level.DEBUG:
-		_write('DEBUG:', data)
+		_write(Level.DEBUG, 'DEBUG:', data)
+
 
 func info(data):
 	"""Informational message"""
 	if level <= Level.INFO:
-		_write('INFO:', data)
+		_write(Level.INFO, 'INFO:', data)
+
 
 func warning(data):
 	"""Warning message"""
 	if level <= Level.WARNING:
-		_write('WARN:', data)
+		_write(Level.WARNING, 'WARN:', data)
+
 
 func error(data):
 	"""Error message"""
 	if level <= Level.ERROR:
-		_write('ERROR:', data)
+		_write(Level.ERROR, 'ERROR:', data)
+
 
 func critical(data):
 	"""Critical error message"""
-	_write('CRIT:', data)
+	_write(Level.CRITICAL, 'CRIT:', data)
 
-func _write(type, data):
+
+func _write(lvl, typef, data):
 	"""Actually write out the message string"""
 	if typeof(data) != TYPE_STRING:
 		data = var2str(data)
 
-	var message = '%s%5s %s' % [time_format_func.call_func(), type, data]
+	var message = '%s%5s %s' % [time_format_func.call_func(), typef, data]
 
 	if print_stdout:
 		if $"/root".has_node("Console"):
-			Console.write_line(message)
+			Console.call_deferred("write_line", message)
 		else:
-			print(message)
+			if lvl == Level.CRITICAL:
+				assert(false, message)
+				printerr(message)
+				print_stack()
+			elif lvl == Level.ERROR:
+				push_error(message)
+				printerr(message)
+			elif lvl == Level.WARNING:
+				push_warning(message)
+				printerr(message)
+			else:
+				print(message)
 		
 	if file != null:
 		file.store_line(message)
