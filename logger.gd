45fb7e9f46976731510a066e45eed55ae67d3a66
sdggames <cicd@sdggamecollective.com>
2022-10-07 02:34:59 -0500

    4.0 Upgrade first pass

--- logger.gd
+++ logger.gd
@@ -48,7 +48,7 @@
 #     Logging to a file (set to 'null' to close the file):
 #         logger.file_name = 'user://log.txt'
 #        
-#     Logging messages of various types (will use var2str() to output any non-string being logged):
+#     Logging messages of various types (will use var_to_str() to output any non-string being logged):
 #         logger.info("Creating a new fish object")
 #         logger.debug([my_vector3, my_vector2, my_list])
 #         logger.warning("Tried to take over the moon!")
@@ -75,60 +75,63 @@ func _ready():
 
 
 # Print to stdout?
-var print_stdout = true setget set_print_stdout, get_print_stdout
-func get_print_stdout():
-	return print_stdout
-func set_print_stdout(value):
-	assert(value in [true, false])
-	print_stdout = value
-
+var print_stdout = true :
+	get:
+		return print_stdout
+	set(value):
+		assert(value in [true, false])
+		print_stdout = value
 
 # Logging level.
-var level = Level.INFO setget set_level, get_level
-func get_level():
-	return level
+var level = Level.INFO :
+	get:
+		return level
+	set(value):
+		level = value
+		assert(value in [Level.DEBUG, Level.INFO, Level.WARNING, Level.ERROR, Level.CRITICAL])
 func set_level(value):
-	assert(level in [Level.DEBUG, Level.INFO, Level.WARNING, Level.ERROR, Level.CRITICAL])
 	level = value
 
 
 # Logging to file.
 var file = null
-var file_name = null setget set_filename, get_filename
-func get_filename():
-	return file_name
-func set_filename(value):
-	if file != null:
-		info("Stopped logging to file: %s" % file_name)
-		file.close()
+var file_name = null :
+	get:
+		return file_name
+	set(value):
+		if file != null:
+			info("Stopped logging to file: %s" % file_name)
+			file.close()
 
-	if value != null:
-		file = File.new()
-		file_name = value
-		file.open(file_name, File.WRITE)
-		info("Started logging to file: %s" % file_name)
-	else:
-		file = null
-		file_name = null  
+		if value != null:
+			file = File.new()
+			file_name = value
+			file.open(file_name, File.WRITE)
+			info("Started logging to file: %s" % file_name)
+		else:
+			file = null
+			file_name = null  
 
 # Log timer
-var time_format_func = funcref(self, "format_time_datetime") setget set_time_format_func
+var time_format_func = format_time_datetime
 func set_time_format_func(value):
 	time_format_func = value
 
 
-var time_format = TimeFormat.DATETIME setget set_time_format
-func set_time_format(value):
-	if value == TimeFormat.NONE:
-		self.time_format_func = funcref(self, "format_time_none")
-	elif value == TimeFormat.DATETIME:
-		self.time_format_func = funcref(self, "format_time_datetime")
-	elif value == TimeFormat.TIME:
-		self.time_format_func = funcref(self, "format_time_time")
-	elif value == TimeFormat.ELAPSED:
-		self.time_format_func = funcref(self, "format_time_elapsed")
-	else:
-		assert(false) # Bad time format used.
+var time_format = TimeFormat.DATETIME :
+	get:
+		return time_format
+	set(value):
+		if value == TimeFormat.NONE:
+			self.time_format_func = format_time_none
+		elif value == TimeFormat.DATETIME:
+			self.time_format_func = format_time_datetime
+		elif value == TimeFormat.TIME:
+			self.time_format_func = format_time_time
+		elif value == TimeFormat.ELAPSED:
+			self.time_format_func = format_time_elapsed
+		else:
+			assert(false) # Bad time format used.
 
 
 # --- Time formatters for use by the logger.
@@ -151,16 +154,16 @@ func format_time_datetime():
 # --- General time formatting functions
 func _format_time():
 	"""Not used directly, but might come in useful"""
-	var time = OS.get_time()
-	# This is not "correct", but gives impression of ms moving on!
-	var ms = OS.get_ticks_msec() % 1000
+	var time = Time.get_datetime_dict_from_system()
+	# This is not "correct", but gives impression of ms moving checked!
+	var ms = Time.get_ticks_msec() % 1000
 
 	return "%02d:%02d:%02d.%03d" % [time["hour"], time["minute"], time["second"], ms]
 
 
 func _format_elapsed():
 	"""Not used directly, but might come in useful"""
-	var time = OS.get_ticks_msec()
+	var time = Time.get_ticks_msec()
 	var ms = time % 1000
 	var s = int(time / 1000) % 60
 	var m = int(time / 60000) % 60
@@ -171,7 +174,7 @@ func _format_elapsed():
 
 func _format_date():
 	"""Not used directly, but might come in useful"""
-	var date = OS.get_date()
+	var date = Time.get_date_dict_from_system()
 
 	return "%d-%02d-%02d" % [date["year"], date["month"], date["day"]]
 
@@ -209,16 +212,16 @@ func critical(data):
 func _write(lvl, typef, data):
 	"""Actually write out the message string"""
 	if typeof(data) != TYPE_STRING:
-		data = var2str(data)
+		data = var_to_str(data)
 
-	var message = '%s%5s %s' % [time_format_func.call_func(), typef, data]
+	var message = '%s%5s %s' % [time_format_func.call(), typef, data]
 
 	if print_stdout:
 		if $"/root".has_node("Console"):
 			Console.call_deferred("write_line", message)
 		else:
 			if lvl == Level.CRITICAL:
-				assert(false, message)
+				assert(false) #,message)
 				printerr(message)
 				print_stack()
 			elif lvl == Level.ERROR:
